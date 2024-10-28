b24eb3e166f114e284474adf72b8357453f6eb9e
Frank Sikutwa <150024292+Lastnameissikutwa@users.noreply.github.com>
2024-10-28 10:22:02 +0200

    Add files via upload

---
+++ user_input.py
@@ -0,0 +1,10 @@
+
+# user_input.py
+
+# Asking for user inputs
+name = input("Please enter your name: ")
+age = input("Please enter your age: ")
+location = input("Please enter your location: ")
+
+# Printing a personalized message
+print(f"Hello {name}, you are {age} years old and live in {location}.")
